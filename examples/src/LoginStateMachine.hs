473c2ddb240de00c60e761891d6880de76ba47e6
Marcin Szamotulski <profunctor@pm.me>
2018-12-10 18:52:59 +0100

    Added Control.Category.Lifting to free-algebras

--- examples/src/LoginStateMachine.hs
+++ examples/src/LoginStateMachine.hs
@@ -1,87 +1,21 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
 module LoginStateMachine where
 
 import Prelude hiding (id, (.))
 
 import Control.Arrow (Kleisli (..))
-import Control.Category (Category (..), (<<<))
+import Control.Category (Category (..))
 import Data.Functor.Identity (Identity (..))
 
-import Control.Category.Free (Cat (..), FreeAlgebra2 (..))
-import Data.Algebra.Free (AlgebraType, AlgebraType0, proof)
+import Control.Category.Free (Cat)
 
-{-------------------------------------------------------------------------------
--- Categories with monadic actions
--------------------------------------------------------------------------------}
-
-class (Category c) => Lifting c m | c -> m where
-  lift :: m (c a b) -> c a b
-
--- | Category transformer, which adds @'Lifting'@ instance to the underlying
--- base category.
-data FreeLifting :: (* -> *) -> (k -> k -> *) -> k -> k -> * where
-  Base :: c a b -> FreeLifting m c a b
-  Lift :: m (FreeLifting m c a b) -> FreeLifting m c a b
-
--- | Wrap a transition into a free category @'Cat'@ and then in
--- @'FreeLifting'@
---
--- prop> liftCat tr = Base (tr :.: Id)
---
-liftCat :: Monad m => tr a b -> FreeLifting m (Cat tr) a b
-liftCat = liftFree2 . liftFree2
-
- -- | Fold @'FreeLifing'@ category based on a free category @'Cat' tr@ using
- -- a natural transformation @tr x y -> c x y@.
- --
-foldNatLift
-  :: (Monad m, Lifting c m)
-  => (forall x y. tr x y -> c x y)
-  -> FreeLifting m (Cat tr) x y
-  -> c x y
-foldNatLift nat = foldNatFree2 (foldNatFree2 nat)
-
-instance (Functor m, Category c) => Category (FreeLifting m c) where
-  id = Base id
-  Base f . Base g = Base (f . g)
-  Lift (mf) . g   = Lift ((. g) <$> mf)
-
--- | This means that one can interpret `AccessCat` in any Kleisli category!
---
-instance Monad m => Lifting (Kleisli m) m where
-  lift m = Kleisli (\a -> m >>= \(Kleisli f) -> f a)
-
-instance (Functor m, Category c) => Lifting (FreeLifting m c) m where
-  lift = Lift
-
-instance Lifting (->) Identity where
-  lift = runIdentity
-
-type instance AlgebraType0 (FreeLifting m) c = (Monad m, Category c)
-type instance AlgebraType  (FreeLifting m) c  = Lifting c m
-instance Monad m => FreeAlgebra2 (FreeLifting m) where
-  liftFree2    = Base
-  foldNatFree2 nat (Base cab)  = nat cab
-  foldNatFree2 nat (Lift mcab) = lift $ foldNatFree2 nat <$> mcab
-
-  codom2  = proof
-  forget2 = proof
-
--- |  Functor from @'->'@ category to @'Kleisli' m@.  If @m@ is @Identity@ then
--- it will respect @'lift'@ i.e. @lfitKleisli (lift ar) = lift (liftKleisli <$>
--- ar).
---
-liftKleisli :: Applicative m => (a -> b) -> Kleisli m a b
-liftKleisli f = Kleisli (pure . f)
+-- Import classes and combintators used in this example
+import Control.Category.Lifting
 
 {-------------------------------------------------------------------------------
 -- Example State Machine, inspired by:
