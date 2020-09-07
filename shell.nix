6ef484524f04bb7189558dc1e8aeac65d18dea81
Manuel Bärenz <m.baerenz@sonnen.de>
2020-09-07 21:10:33 +0200

    Reduce shell.nix to minimal working implementation

--- shell.nix
+++ shell.nix
@@ -1,19 +1,4 @@
-{ compiler ? "ghc865"
-, haddock ? true
-, test ? true
-, benchmarks ? false
-, dev ? false
-}:
-with builtins;
 let
-  default = import ./default.nix {inherit benchmarks compiler dev haddock test;};
-  nixpkgs = import ./nix/nixpkgs.nix { inherit compiler; };
+  default = import ./default.nix {};
 in
-  {
-    free-category = if nixpkgs.lib.inNixShell
-      then default.free-category.env
-      else default.free-category;
-    examples = if nixpkgs.lib.inNixShell
-      then default.examples.env
-      else default.examples;
-  }
+default.shellFor
